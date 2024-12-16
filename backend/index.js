4ac6ce7e8b05dc54fa63d0bf3a8e46c71a6b7009
Cem <bcemcankaya@gmail.com>
2024-12-16 20:48:03 +0300

    add cookie-parser

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const cityRoutes = require('./routes/cityRoutes');
 const adRoutes = require('./routes/adRoutes');
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 
@@ -11,11 +12,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/cities', cityRoutes);
-app.use("/api/ads", adRoutes);
-
+app.use('/api/ads', adRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
