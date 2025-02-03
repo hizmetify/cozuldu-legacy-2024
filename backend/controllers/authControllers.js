d72cf1d97351175dc6c23d5a87bbacbbec8d5805
Cem <bcemcankaya@gmail.com>
2025-02-03 18:52:55 +0300

    change the httpOnly setting for development

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -65,7 +65,7 @@ const register = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
@@ -105,7 +105,7 @@ const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
