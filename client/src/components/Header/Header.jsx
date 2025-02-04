f0e73d9c4ff9588858bf84ead05e5f6e3da6a4fb
Cem <bcemcankaya@gmail.com>
2025-02-04 14:52:00 +0300

    feat: add mobile menu toggle functionality

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdDashboard } from 'react-icons/md';
 import { FaBars } from 'react-icons/fa6';
 import CategoryDropdown from './CategoryDropdown';
+import { IoCloseOutline, IoMenuOutline } from 'react-icons/io5';
 
 const Header = () => {
+  const [openMobile, setOpenMobile] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenMobile(!openMobile);
+  };
   return (
     <header className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -29,7 +36,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/register"
-                  className=" bg-gradient-to-r from-blue-500 to-blue-700 text-white hover:from-blue-600 hover:to-blue-800 px-4 py-2 rounded-sm text-sm font-medium transition duration-150 ease-in-out"
+                  className=" bg-gradient-to-r from-blue-500 to-blue-700 text-white hover:from-blue-600 hover:to-blue-800 px-4 py-2.5 rounded text-sm font-medium transition duration-150 ease-in-out"
                 >
                   Hizmet Ver
                 </Link>
@@ -37,33 +44,34 @@ const Header = () => {
             </ul>
           </nav>
           <div className="md:hidden">
-            <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:gray-blue-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
+            <button
+              onClick={toggleNavbar}
+              className="inline-flex items-center text-xl justify-center p-2 rounded-md text-gray-700 hover:gray-blue-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500"
+            >
               <span className="sr-only">Open main menu</span>
-              <FaBars />
+              {openMobile ? <IoCloseOutline /> : <IoMenuOutline />}
             </button>
           </div>
         </div>
       </div>
-      <div className="md:hidden">
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <button className="flex items-center w-full px-3 py-2 text-base font-medium text-blue-700 rounded-sm hover:text-blue-600 hover:bg-blue-100">
-            <span>Kategoriler</span>
-            <MdDashboard className="ml-2 text-blue-600 text-lg" />
-          </button>
-          <Link
-            to="/login"
-            className="block px-3 py-2 rounded-md text-base font-medium text-blue-700 hover:text-blue-600 hover:bg-blue-100"
-          >
-            Giriş Yap
-          </Link>
-          <Link
-            to="/register"
-            className="block px-3 py-2 text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
-          >
-            Hizmet Ver
-          </Link>
+      {openMobile && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              to="/login"
+              className="block px-3 py-2 rounded-md text-base font-medium text-blue-700 hover:text-blue-600"
+            >
+              Giriş Yap
+            </Link>
+            <Link
+              to="/register"
+              className="block px-3 py-2 text-base font-medium text-whit text-blue-700 hover:text-blue-600"
+            >
+              Hizmet Ver
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </header>
   );
 };
