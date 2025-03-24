a5eecb40e140dbbcbacc3e7e3ccaf02f401b69f5
Cem <bcemcankaya@gmail.com>
2025-03-24 13:44:06 +0300

    implement redux-persist for better ux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
-import { store } from './store.js';
+import { persistor, store } from './store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import LoadingSpinner from './components/UI/LoadingSpinner.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <Toaster position="top-right" />
-      <App />
+      <PersistGate loading={<LoadingSpinner />} persistor={persistor}>
+        <Toaster position="top-right" />
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authReducer from './features/auth/authSlice';
 import registerReducer from './features/register/registerSlice';
 import sidebarReducer from './features/sidebar/sidebarSlice';
@@ -7,14 +9,28 @@ import toastReducer from './features/toast/toastSlice';
 import userReducer from './features/user/userSlice';
 import loadingReducer from './features/loading/loadingSlice';
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    register: registerReducer,
-    sidebar: sidebarReducer,
-    ads: adsReducer,
-    toast: toastReducer,
-    user: userReducer,
-    loading: loadingReducer,
-  },
+const rootReducer = combineReducers({
+  auth: authReducer,
+  register: registerReducer,
+  sidebar: sidebarReducer,
+  ads: adsReducer,
+  toast: toastReducer,
+  user: userReducer,
+  loading: loadingReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
